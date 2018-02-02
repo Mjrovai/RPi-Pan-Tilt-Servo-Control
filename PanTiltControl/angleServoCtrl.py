1d7edfab181f5d9183e5f4cdc3badadb8f475c6f
Marcelo Rovai <Mjrovai@gmail.com>
2018-02-02 15:07:40 -0300

    Update angleServoCtrl.py

--- PanTiltControl/angleServoCtrl.py
+++ PanTiltControl/angleServoCtrl.py
@@ -11,7 +11,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 def setServoAngle(servo, angle):
-	assert angle >=30 and angle <= 150
 	pwm = GPIO.PWM(servo, 50)
 	pwm.start(8)
 	dutyCycle = angle / 18. + 3.
